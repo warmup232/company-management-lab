11f814880627de779aeaa3c8d855410dcf286bc0
warmup232 <tglee716@gmail.com>
2026-08-26 15:03:14 +0900

    Create products table in schema.sql

---
+++ mysql/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products (
+    product_id INT,
+    product_name VARCHAR(100),
+    price INT,
+    stock_quantity INT,
+    category VARCHAR(50)
+);
+product_id INT AUTO_INCREMENT PRIMARY KEY,
